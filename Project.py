6e47e95af6657875be546d079d6c88bb6869d7d9
Nnamdi Nwaenia <nnamdinwaenia@Nnamdis-MacBook-Air.local>
2025-04-09 16:28:28 -0400

    This is the other part of the starter code more to follow

--- Project.py
+++ Project.py
@@ -9,3 +9,6 @@ vpic_data = json.loads(vpic_response.text)['Results']
 carquery_api_url = "https://www.carqueryapi.com/api/0.3/?cmd=getMakes&year=2015"
 carquery_response = requests.get(carquery_api_url)
 carquery_data = json.loads(carquery_response.text)['Makes']
+
+vpic_makes = [d['Make_Name'] for d in vpic_data]
+carquery_makes = [d['make_display'] for d in carquery_data]
