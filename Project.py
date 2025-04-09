5867867d219af0201a9717cce694b7779587a183
Nnamdi Nwaenia <nnamdinwaenia@Nnamdis-MacBook-Air.local>
2025-04-09 16:25:48 -0400

    Just adding some of the new updates of our project

--- Project.py
+++ Project.py
@@ -1,1 +1,11 @@
+import requests
+import json
+import matplotlib.pyplot as plt
 
+vpic_api_url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
+vpic_response = requests.get(vpic_api_url)
+vpic_data = json.loads(vpic_response.text)['Results']
+
+carquery_api_url = "https://www.carqueryapi.com/api/0.3/?cmd=getMakes&year=2015"
+carquery_response = requests.get(carquery_api_url)
+carquery_data = json.loads(carquery_response.text)['Makes']
