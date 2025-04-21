139ec468d94478be6f48dbd659e344bb2c538e1a
James Aretakis <jamesaretakis@Jamess-MacBook-Pro-33.local>
2025-04-21 16:22:47 -0400

    added new API

--- Project.py
+++ Project.py
@@ -2,13 +2,13 @@ import requests
 import json
 import matplotlib.pyplot as plt
 
-vpic_api_url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
-vpic_response = requests.get(vpic_api_url)
-vpic_data = json.loads(vpic_response.text)['Results']
+crash_api_url = "https://crashviewer.nhtsa.dot.gov/CrashAPI/crashes/GetCaseList?State=NY&Year=2021&format=json"
+crash_response = requests.get(crash_api_url)
+crash_data = json.loads(crash_response.text)['Results']
 
 carquery_api_url = "https://www.carqueryapi.com/api/0.3/?cmd=getMakes&year=2015"
 carquery_response = requests.get(carquery_api_url)
 carquery_data = json.loads(carquery_response.text)['Makes']
 
-vpic_makes = [d['Make_Name'] for d in vpic_data]
+vpic_makes = [d['Make_Name'] for d in crash_data]
 carquery_makes = [d['make_display'] for d in carquery_data]
